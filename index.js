160ccadaf860e693461e9786b55eaabe8588309b
sreddy3@cox.net Reddy <sreddy3@cox.net>
2018-09-06 16:14:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,23 +2,25 @@ var kittens = ['Milo', 'Otis', 'Garfield']// Add your functions and code here
 
 function destructivelyAppendKitten(name){
   kittens = kittens.concat('Ralph')
-  return kittens
+    return kittens
 }
 
 function destructivelyPrependKitten(name){
   kittens = ['Bob', ...kittens]
-  return kittens
+    return kittens
 }
 
 function destructivelyRemoveLastKitten(){
   kittens = kittens.slice(0, 2)
-  return kittens
+    return kittens
 }
 function destructivelyRemoveFirstKitten(){
   kittens = kittens.splice(1)
-  return kittens
+    return kittens
 }
+
 function appendKitten(name){
-  kittens = kittens.slice(0, 'Broom')
-  return kittens
+  kittens = kittens.concat('Broom')
+    return kittens
 }
+
